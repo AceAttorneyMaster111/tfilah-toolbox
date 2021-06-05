f390517f751b4b0fe4d010fe3ee76e6326bd99df
AceAttorneyMaster111 <NoahS2003@gmail.com>
2021-06-05 17:11:08 -0400

    Added order by field

--- service_generator/views.py
+++ service_generator/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Song
 
 def search(request):
-    return render(request, "service_generator/search.html", {"song_list": Song.objects.all, "filter_text": ""})
+    return render(request, "service_generator/search.html", {"song_list": Song.objects.all, "filter_text": "", "order_by": "title"})
 
 def filter(request):
     filter_by = {
@@ -23,9 +23,9 @@ def filter(request):
         | (Q(artist__icontains=filter_text) if filter_by["artist"] else Q())
         | (Q(prayer__name__icontains=filter_text) if filter_by["prayer"] else Q())
     )
-    song_list = Song.objects.filter(query)
+    song_list = Song.objects.filter(query).order_by(request.GET["order_by"])
 
-    return render(request, "service_generator/search.html", {"song_list": song_list, "filter_text": filter_text})
+    return render(request, "service_generator/search.html", {"song_list": song_list, "filter_text": filter_text, "order_by": request.GET["order_by"]})
 
 def viewsong(request, song_id):
     song = get_object_or_404(Song, pk=song_id)
