ab631b8ad9bb03c5b00b8258d48a1ff66a9a4d7f
AceAttorneyMaster111 <NoahS2003@gmail.com>
2021-06-05 12:49:58 -0400

    It works!

--- service_generator/urls.py
+++ service_generator/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "service_generator"
 urlpatterns = [
     path("", views.search, name="search"),
-    path("viewsong/<int:song_id>/", views.viewsong, name="viewsong")
+    path("viewsong/<int:song_id>/", views.viewsong, name="viewsong"),
+    path("viewsong/<int:song_id>/download_chordpro/", views.download_chordpro, name="download_chordpro"),
+    path("viewsong/<int:song_id>/download_pdf/", views.download_pdf, name="download_pdf")
 ]
--- service_generator/views.py
+++ service_generator/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from chopro import chopro2html
+
+from django.http import FileResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Song
 
@@ -7,4 +9,14 @@ def search(request):
     return render(request, "service_generator/search.html", {"song_list": Song.objects.all})
 
 def viewsong(request, song_id):
-    return HttpResponse(f"You are looking at song {song_id}.")
+    song = get_object_or_404(Song, pk=song_id)
+    song.chordsheet.open("r")
+    chordsheet_html = chopro2html(song.chordsheet.read())
+    song.chordsheet.close()
+    return render(request, "service_generator/viewsong.html", {"song": song, "chordsheet_html": chordsheet_html})
+
+def download_chordpro(request, song_id):
+    return FileResponse(get_object_or_404(Song, pk=song_id).chordsheet, as_attachment=True)
+
+def download_pdf(request, song_id):
+    pass 
