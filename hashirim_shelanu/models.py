5bd03da5f2f1b912778d82def36ecb0a632d3c2a
AceAttorneyMaster111 <NoahS2003@gmail.com>
2021-08-10 18:36:09 -0400

    Deleted old model

--- hashirim_shelanu/models.py
+++ hashirim_shelanu/models.py
@@ -13,20 +13,6 @@ class Prayer(models.Model):
     def __str__(self):
         return self.name
 
-class Service_Type(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
-    prayers = models.ManyToManyField(Prayer, through="Prayer_Position", related_name="+")
-
-    def __str__(self):
-        return self.name
-
-class Prayer_Position(models.Model):
-    prayer = models.ForeignKey(Prayer, on_delete=models.CASCADE, related_name="+")
-    service_type = models.ForeignKey(Service_Type, on_delete=models.CASCADE, related_name="+")
-    index = models.PositiveSmallIntegerField(unique=True)
-    # TODO: Deal with the fact that this is ordered
-
 class Artist(models.Model):
     name = models.CharField(max_length=200)
 
