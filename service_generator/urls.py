59bc987b72f883cfa324798425eaf80891fca72f
AceAttorneyMaster111 <NoahS2003@gmail.com>
2022-07-19 16:19:13 -0400

    fixed another bug, nothing to see here

--- service_generator/urls.py
+++ service_generator/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = "service_generator"
 urlpatterns = [
-    path("viewservicetype/<int:service_id>", views.view_service_type, name="view_service_type"),
     path("", views.landing, name="landing"),
-    path("createservice/<int:service_id>", views.create_service, name="create_service")
+    path("viewservicetype/<int:service_id>", views.view_service_type, name="view_service_type")
 ]
