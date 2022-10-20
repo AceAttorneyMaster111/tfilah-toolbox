5860c1585defcd15ec6f046fc848e6e20f3b9962
AceAttorneyMaster111 <NoahS2003@gmail.com>
2022-10-20 18:54:23 -0400

    Started work on beginservice.html view

--- service_generator/urls.py
+++ service_generator/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "service_generator"
 urlpatterns = [
     path("", views.landing, name="landing"),
+    path("beginservice/<int:service_id>", views.begin_service, name="begin_service"),
     path("viewservicetype/<int:service_id>", views.view_service_type, name="view_service_type")
 ]
--- service_generator/views.py
+++ service_generator/views.py
@@ -1,11 +1,19 @@
+from xml.etree import ElementTree
+
 from django.shortcuts import get_object_or_404, render
 
 from .models import ServiceType
 
 # Create your views here.
 def landing(request):
-    return render(request, "service_generator/landing.html", {"service_types": Service_Type.objects.all})
+    return render(request, "service_generator/landing.html", {"service_types": ServiceType.objects.all})
+
+def begin_service(request, service_id):
+    return render(request, "service_generator/beginservice.html", {"service_type": get_object_or_404(ServiceType, pk=service_id)})
+
+def generate_template(request, args):
+    service = ElementTree.Element("service")
 
 def view_service_type(request, service_id):
-    service = get_object_or_404(Service_Type, pk=service_id)
+    service = get_object_or_404(ServiceType, pk=service_id)
     return render(request, "service_generator/viewservicetype.html", {"service_type": service})
